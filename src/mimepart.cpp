abee386f963e33b889660ef241fa0c67d4848997
Daniel Wendt <daniel.wendt@linux.com>
2024-07-01 15:40:57 +0200

    add flag for already Base64 encoded content

--- src/mimepart.cpp
+++ src/mimepart.cpp
@@ -45,6 +45,7 @@ MimePart::MimePart(const MimePart &other) : d_ptr(new MimePartPrivate)
     d->contentType = other.contentType();
     d->contentEncoding = other.encoding();
     d->header = other.header();
+    d->contentIsBase64 = other.contentIsBase64();
 }
 
 MimePart::~MimePart()
@@ -68,6 +69,7 @@ MimePart &MimePart::operator=(const MimePart &other)
     d->contentType = other.contentType();
     d->contentEncoding = other.encoding();
     d->header = other.header();
+    d->contentIsBase64 = other.contentIsBase64();
 
     return *this;
 }
@@ -276,6 +278,18 @@ bool MimePart::write(QIODevice *device)
     return writeData(device);
 }
 
+void MimePart::setContentIsBase64(bool isBase64)
+{
+    Q_D(MimePart);
+    d->contentIsBase64 = isBase64;
+}
+
+bool MimePart::contentIsBase64() const
+{
+    Q_D(const MimePart);
+    return d->contentIsBase64;
+}
+
 MimePart::MimePart(MimePartPrivate *d) : d_ptr(d)
 {
 
@@ -303,8 +317,17 @@ bool MimePart::writeData(QIODevice *device)
         }
         break;
     case MimePart::Base64:
-        if (!d->writeBase64(input, device)) {
-            return false;
+        if(!d->contentIsBase64)
+        {
+            if (!d->writeBase64(input, device)) {
+                return false;
+            }
+        }
+        else
+        {
+            if (!d->writeRaw(input, device)) {
+                return false;
+            }
         }
         break;
     case MimePart::QuotedPrintable:
